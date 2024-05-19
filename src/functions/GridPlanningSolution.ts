0ac4aecd26ddf4e1236cfd813d65e7063a3f9c6e
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-05-19 12:56:05 +0300

    update

--- src/functions/GridPlanningSolution.ts
+++ src/functions/GridPlanningSolution.ts
@@ -8,257 +8,314 @@ export class Task {
   isVirtual: boolean;
 
   constructor(
-      taskId: number,
-      duration: number,
-      resources: number,
-      previous: number[] = [],
-      earlyStart: number = 0,
-      lateFinish: number = 0,
-      isVirtual: boolean = false
+    taskId: number,
+    duration: number,
+    resources: number,
+    previous: number[] = [],
+    earlyStart: number = 0,
+    lateFinish: number = 0,
+    isVirtual: boolean = false
   ) {
-      this.taskId = taskId;
-      this.previous = previous;
-      this.duration = duration;
-      this.resources = resources;
-      this.earlyStart = earlyStart;
-      this.lateFinish = lateFinish;
-      this.isVirtual = isVirtual;
+    this.taskId = taskId;
+    this.previous = previous;
+    this.duration = duration;
+    this.resources = resources;
+    this.earlyStart = earlyStart;
+    this.lateFinish = lateFinish;
+    this.isVirtual = isVirtual;
   }
 
   get earlyFinish(): number {
-      return this.earlyStart + this.duration;
+    return this.earlyStart + this.duration;
   }
 
   get lateStart(): number {
-      return this.lateFinish - this.duration;
+    return this.lateFinish - this.duration;
   }
 
   get reserveTime(): number {
-      return this.lateFinish - this.earlyFinish;
+    return this.lateFinish - this.earlyFinish;
   }
 }
 
+export function optimalSolution(tasksList: Task[]): Task[] {
+  const tasks = makeStartTasksList(tasksList);
+  console.log("Constructed list of starting tasks");
 
-  function findOptimalSolution(tasksList: Task[]): Task[] {
-      const tasks = makeStartTasksList(tasksList);
-      console.log("Constructed list of starting tasks");
+  calculateEarlyStarts(tasks);
+  console.log("Calculated early starts");
 
-      calculateEarlyStarts(tasks);
-      console.log("Calculated early starts");
+  calculateLateFinishes(tasks);
+  console.log("Calculated late finishes");
 
-      calculateLateFinishes(tasks);
-      console.log("Calculated late finishes");
+  return tasks;
+}
 
-      return tasks;
+export function printTaskList(tasksList: Task[]): void {
+  tasksList = [...tasksList].sort((a, b) => a.taskId - b.taskId);
+  console.log("______ TASKS ______");
+  tasksList.forEach((task) => {
+    console.log(`TASK ${task.taskId}`);
+    console.log(`Duration     ${task.duration}`);
+    console.log(`Resources    ${task.resources}`);
+    console.log(`Early start  ${task.earlyStart}`);
+    console.log(`Early finish ${task.earlyFinish}`);
+    console.log(`Late start   ${task.lateStart}`);
+    console.log(`Late finish  ${task.lateFinish}`);
+    console.log(`Reserve time ${task.reserveTime}`);
+    console.log();
+  });
+
+  const criticalTasks = getCriticalTaskChain(tasksList);
+
+  let criticalTasksPath = "";
+  criticalTasks.forEach((task) => {
+    if (!task.isVirtual) {
+      criticalTasksPath += `${task.taskId} - `;
+    }
+  });
+  criticalTasksPath = criticalTasksPath.substring(
+    0,
+    criticalTasksPath.length - 3,
+  );
+  console.log("Critical path");
+  console.log(criticalTasksPath);
+  console.log("Project duration");
+  console.log(Math.max(...criticalTasks.map((task) => task.earlyFinish)));
+}
+
+export function printLoadChart(tasksList: Task[]): void {
+  const tasks = tasksList.filter((task) => !task.isVirtual);
+  const maxWidth = Math.max(...tasks.map((task) => task.earlyFinish));
+
+  const timeToLoad: [number, number][] = [];
+
+  for (let time = 0; time <= maxWidth; time++) {
+    const fitTasks = tasks.filter(
+      (task) => time >= task.earlyStart && time < task.earlyFinish,
+    );
+    const load = fitTasks.reduce((sum, task) => sum + task.resources, 0);
+
+    // console.log(
+    //   `Time: ${time} Load: ${load} Tasks: ${fitTasks.map((task) => task.taskId).join(", ")}`,
+    // );
+    timeToLoad.push([time, load]);
   }
 
-  function printTaskList(tasksList: Task[]): void {
-      tasksList = [...tasksList].sort((a, b) => a.taskId - b.taskId);
-      console.log("______ TASKS ______");
-      tasksList.forEach(task => {
-          console.log(`TASK ${task.taskId}`);
-          console.log(`Duration     ${task.duration}`);
-          console.log(`Resources    ${task.resources}`);
-          console.log(`Early start  ${task.earlyStart}`);
-          console.log(`Early finish ${task.earlyFinish}`);
-          console.log(`Late start   ${task.lateStart}`);
-          console.log(`Late finish  ${task.lateFinish}`);
-          console.log(`Reserve time ${task.reserveTime}`);
-          console.log();
-      });
+  const maxHeight = Math.max(...timeToLoad.map(([_, load]) => load));
 
-      const criticalTasks = getCriticalTaskChain(tasksList);
+  console.log("_____ Load chart _____");
 
-      let criticalTasksPath = "";
-      criticalTasks.forEach(task => {
-          if (!task.isVirtual) {
-              criticalTasksPath += `${task.taskId} - `;
-          }
-      });
-      criticalTasksPath = criticalTasksPath.substring(0, criticalTasksPath.length - 3);
-      console.log("Critical path");
-      console.log(criticalTasksPath);
-      console.log("Project duration");
-      console.log(Math.max(...criticalTasks.map(task => task.earlyFinish)));
-  }
+  const maxLabelLength = Math.max(
+    ...timeToLoad.map(([time]) => time.toString().length),
+  );
+  const labelFormat = (day: number) =>
+    day.toString().padStart(maxLabelLength, " ");
 
-  function printLoadChart(tasksList: Task[]): void {
-      const tasks = tasksList.filter(task => !task.isVirtual);
-      const maxWidth = Math.max(...tasks.map(task => task.earlyFinish));
-
-      const timeToLoad: [number, number][] = [];
-
-      for (let time = 0; time <= maxWidth; time++) {
-          const fitTasks = tasks.filter(task => time >= task.earlyStart && time < task.earlyFinish);
-          const load = fitTasks.reduce((sum, task) => sum + task.resources, 0);
-
-          console.log(`Time: ${time} Load: ${load} Tasks: ${fitTasks.map(task => task.taskId).join(', ')}`);
-          timeToLoad.push([time, load]);
+  // Print chart by rows
+  for (let height = maxHeight; height >= 0; height--) {
+    for (const [, dayHeight] of timeToLoad) {
+      if (dayHeight > height) {
+        process.stdout.write("*".padEnd(maxLabelLength + 1, " "));
+      } else {
+        process.stdout.write(" ".repeat(maxLabelLength + 1));
       }
-
-      const maxHeight = Math.max(...timeToLoad.map(([_, load]) => load));
-
-      console.log("_____ Load chart _____");
-
-      const maxLabelLength = Math.max(...timeToLoad.map(([time]) => time.toString().length));
-      const labelFormat = (day: number) => day.toString().padStart(maxLabelLength, ' ');
-
-      // Print chart by rows
-      for (let height = maxHeight; height >= 0; height--) {
-          for (const [, dayHeight] of timeToLoad) {
-              if (dayHeight > height) {
-                  process.stdout.write("*".padEnd(maxLabelLength + 1, ' '));
-              } else {
-                  process.stdout.write(" ".repeat(maxLabelLength + 1));
-              }
-          }
-          console.log();
-      }
-
-      // Print axis
-      for (const [day] of timeToLoad) {
-          process.stdout.write(labelFormat(day) + ' ');
-      }
-      console.log();
+    }
+    console.log();
   }
 
-  function getCriticalTaskChain(tasks: Task[]): Task[] {
-      return sortTaskChain(tasks.filter(task => task.reserveTime === 0));
+  // Print axis
+  for (const [day] of timeToLoad) {
+    process.stdout.write(labelFormat(day) + " ");
+  }
+  console.log();
+}
+
+export function getCriticalTaskChain(tasks: Task[]): Task[] {
+  return sortTaskChain(tasks.filter((task) => task.reserveTime === 0));
+}
+
+function sortTaskChain(tasks: Task[]): Task[] {
+  const visitedTasks: Task[] = [];
+  while (visitedTasks.length !== tasks.length) {
+    for (const task of tasks) {
+      if (visitedTasks.some((t) => t.taskId === task.taskId)) continue;
+
+      const nextTasks = tasks.filter((t) => t.previous.includes(task.taskId));
+
+      // if the next ones have not yet been solved.
+      if (
+        !visitedTasks
+          .map((t) => t.taskId)
+          .includesAll(nextTasks.map((t) => t.taskId))
+      )
+        continue;
+      visitedTasks.push(task);
+    }
   }
 
-  function sortTaskChain(tasks: Task[]): Task[] {
-      const visitedTasks: Task[] = [];
-      while (visitedTasks.length !== tasks.length) {
-          for (const task of tasks) {
-              if (visitedTasks.some(t => t.taskId === task.taskId)) continue;
+  return visitedTasks.reverse();
+}
 
-              const nextTasks = tasks.filter(t => t.previous.includes(task.taskId));
+export function findProjectDuration(tasks: Task[]): number {
+  return Math.max(...tasks.map((task) => task.earlyFinish));
+}
 
-              // if the next ones have not yet been solved.
-              if (!visitedTasks.map(t => t.taskId).includesAll(nextTasks.map(t => t.taskId))) continue;
-              visitedTasks.push(task);
-          }
-      }
+function calculateLateFinishes(tasks: Task[]): void {
+  const visitedTasksId: number[] = [];
+  while (visitedTasksId.length !== tasks.length) {
+    for (let i = 0; i < tasks.length; i++) {
+      const task = tasks[i];
+      if (visitedTasksId.includes(task.taskId)) continue;
 
-      return visitedTasks.reverse();
-  }
+      const nextTasks = tasks.filter((t) => t.previous.includes(task.taskId));
 
-  function findProjectDuration(tasks: Task[]): number {
-      return Math.max(...tasks.map(task => task.earlyFinish));
-  }
+      // if the next ones have not yet been solved.
+      if (!visitedTasksId.includesAll(nextTasks.map((t) => t.taskId))) continue;
+      visitedTasksId.push(task.taskId);
 
-  function calculateLateFinishes(tasks: Task[]): void {
-      const visitedTasksId: number[] = [];
-      while (visitedTasksId.length !== tasks.length) {
-          for (let i = 0; i < tasks.length; i++) {
-              const task = tasks[i];
-              if (visitedTasksId.includes(task.taskId)) continue;
-
-              const nextTasks = tasks.filter(t => t.previous.includes(task.taskId));
-
-              // if the next ones have not yet been solved.
-              if (!visitedTasksId.includesAll(nextTasks.map(t => t.taskId))) continue;
-              visitedTasksId.push(task.taskId);
-
-              // if no tasks after
-              if (nextTasks.length === 0) {
-                  tasks[i] = new Task(task.taskId, task.duration, task.resources, task.previous, task.earlyStart, task.earlyFinish, task.isVirtual);
-                  continue;
-              }
-
-              // for handle tasks after
-              const lateFinish = Math.min(...nextTasks.map(t => t.lateStart));
-
-              tasks[i] = new Task(task.taskId, task.duration, task.resources, task.previous, task.earlyStart, lateFinish, task.isVirtual);
-          }
-      }
-  }
-
-  function calculateEarlyStarts(tasks: Task[]): void {
-      const visitedTasksId: number[] = [];
-      while (visitedTasksId.length !== tasks.length) {
-          for (let i = 0; i < tasks.length; i++) {
-              const task = tasks[i];
-              if (visitedTasksId.includes(task.taskId)) continue;
-
-              // if the previous ones have not yet been solved.
-              if (!visitedTasksId.includesAll(task.previous)) continue;
-              visitedTasksId.push(task.taskId);
-
-              // if no tasks before
-              if (task.previous.length === 0) {
-                  tasks[i] = new Task(task.taskId, task.duration, task.resources, task.previous, 0, task.lateFinish, task.isVirtual);
-                  continue;
-              }
-
-              // for handle tasks before
-              const earlyStart = Math.max(...tasks.filter(t => task.previous.includes(t.taskId)).map(t => t.earlyFinish));
-
-              tasks[i] = new Task(task.taskId, task.duration, task.resources, task.previous, earlyStart, task.lateFinish, task.isVirtual);
-          }
-      }
-  }
-
-  function makeStartTasksList(tasksList: Task[]): Task[] {
-      let tasks = deepCopyTasks(tasksList);
-
-      const firstTasks = tasks.filter(task => task.previous.length === 0).map(task => task.taskId);
-
-      // Add first task is needed
-      if (firstTasks.length > 1) {
-          const startTaskId = findUniqueInt(tasks);
-          const startTask = new Task(startTaskId, 0, 0, [], 0, 0, true);
-
-          tasks = tasks.map(task => {
-              if (task.previous.length === 0) {
-                  task.previous = [startTaskId];
-              }
-              return task;
-          });
-
-          tasks = [startTask, ...tasks];
-      }
-
-      // Adding final task is needed
-      const endTasks = tasks.filter(task => tasks.every(t => !t.previous.includes(task.taskId))).map(task => task.taskId);
-
-      if (endTasks.length > 1) {
-          const endTaskId = findUniqueInt(tasks);
-          const endTask = new Task(endTaskId, 0, 0, endTasks, 0, 0, true);
-
-          tasks = [...tasks, endTask];
-      }
-
-      return tasks;
-  }
-
-  function findUniqueInt(tasks: Task[]): number {
-      let id = -1;
-      while (tasks.some(task => task.taskId === id)) {
-          id++;
-      }
-      return id;
-  }
-
-  function deepCopyTasks(tasks: Task[]): Task[] {
-      return tasks.map(task => new Task(
+      // if no tasks after
+      if (nextTasks.length === 0) {
+        tasks[i] = new Task(
           task.taskId,
           task.duration,
           task.resources,
-          [...task.previous],
+          task.previous,
           task.earlyStart,
-          task.lateFinish,
-          task.isVirtual
-      ));
+          task.earlyFinish,
+          task.isVirtual,
+        );
+        continue;
+      }
+
+      // for handle tasks after
+      const lateFinish = Math.min(...nextTasks.map((t) => t.lateStart));
+
+      tasks[i] = new Task(
+        task.taskId,
+        task.duration,
+        task.resources,
+        task.previous,
+        task.earlyStart,
+        lateFinish,
+        task.isVirtual,
+      );
+    }
   }
+}
+
+function calculateEarlyStarts(tasks: Task[]): void {
+  const visitedTasksId: number[] = [];
+  while (visitedTasksId.length !== tasks.length) {
+    for (let i = 0; i < tasks.length; i++) {
+      const task = tasks[i];
+      if (visitedTasksId.includes(task.taskId)) continue;
+
+      // if the previous ones have not yet been solved.
+      if (!visitedTasksId.includesAll(task.previous)) continue;
+      visitedTasksId.push(task.taskId);
+
+      // if no tasks before
+      if (task.previous.length === 0) {
+        tasks[i] = new Task(
+          task.taskId,
+          task.duration,
+          task.resources,
+          task.previous,
+          0,
+          task.lateFinish,
+          task.isVirtual,
+        );
+        continue;
+      }
+
+      // for handle tasks before
+      const earlyStart = Math.max(
+        ...tasks
+          .filter((t) => task.previous.includes(t.taskId))
+          .map((t) => t.earlyFinish),
+      );
+
+      tasks[i] = new Task(
+        task.taskId,
+        task.duration,
+        task.resources,
+        task.previous,
+        earlyStart,
+        task.lateFinish,
+        task.isVirtual,
+      );
+    }
+  }
+}
+
+function makeStartTasksList(tasksList: Task[]): Task[] {
+  let tasks = deepCopyTasks(tasksList);
+
+  const firstTasks = tasks
+    .filter((task) => task.previous.length === 0)
+    .map((task) => task.taskId);
+
+  // Add first task is needed
+  if (firstTasks.length > 1) {
+    const startTaskId = findUniqueInt(tasks);
+    const startTask = new Task(startTaskId, 0, 0, [], 0, 0, true);
+
+    tasks = tasks.map((task) => {
+      if (task.previous.length === 0) {
+        task.previous = [startTaskId];
+      }
+      return task;
+    });
+
+    tasks = [startTask, ...tasks];
+  }
+
+  // Adding final task is needed
+  const endTasks = tasks
+    .filter((task) => tasks.every((t) => !t.previous.includes(task.taskId)))
+    .map((task) => task.taskId);
+
+  if (endTasks.length > 1) {
+    const endTaskId = findUniqueInt(tasks);
+    const endTask = new Task(endTaskId, 0, 0, endTasks, 0, 0, true);
+
+    tasks = [...tasks, endTask];
+  }
+
+  return tasks;
+}
+
+function findUniqueInt(tasks: Task[]): number {
+  let id = -1;
+  while (tasks.some((task) => task.taskId === id)) {
+    id++;
+  }
+  return id;
+}
+
+function deepCopyTasks(tasks: Task[]): Task[] {
+  return tasks.map(
+    (task) =>
+      new Task(
+        task.taskId,
+        task.duration,
+        task.resources,
+        [...task.previous],
+        task.earlyStart,
+        task.lateFinish,
+        task.isVirtual,
+      ),
+  );
+}
 
 // Array.prototype.includesAll polyfill
 declare global {
   interface Array<T> {
-      includesAll(array: T[]): boolean;
+    includesAll(array: T[]): boolean;
   }
 }
 
-Array.prototype.includesAll = function(array: unknown[]): boolean {
-  return array.every(item => this.includes(item));
-}
+Array.prototype.includesAll = function (array: unknown[]): boolean {
+  return array.every((item) => this.includes(item));
+};
---
+++ src/tests/GridPlanningSolution.test.ts
@@ -0,0 +1,100 @@
+import {
+  Task,
+  findProjectDuration,
+  getCriticalTaskChain,
+  optimalSolution,
+  printLoadChart,
+  printTaskList,
+} from "../functions/gridPlanningSolution";
+import { toStrictEqual1Array } from "./toStrictEqualArray";
+
+test("gridPlanningSolution_test-1", () => {
+  const tasks = [
+    new Task(1, 5, 2, []),
+    new Task(2, 8, 3, [1]),
+    new Task(3, 3, 2, [1]),
+    new Task(4, 6, 2, [1]),
+    new Task(5, 7, 3, [2]),
+    new Task(6, 6, 2, [2, 3]),
+    new Task(7, 4, 2, [4, 5, 6]),
+  ];
+
+  const expectedChain = [1, 2, 5, 7];
+  const expectedProjectDuration = 24;
+
+  console.log("Result (test-1):");
+
+  const result = optimalSolution(tasks);
+
+  printTaskList(result);
+  printLoadChart(result);
+
+  const outputCriticalChain = getCriticalTaskChain(result).filter(task => !task.isVirtual).map(task => task.taskId)
+  const projectDuration = findProjectDuration(result)
+
+  expect(toStrictEqual1Array(outputCriticalChain, expectedChain)).toBe(true);
+  expect(projectDuration).toBe(expectedProjectDuration);
+});
+
+test("gridPlanningSolution_test-2", () => {
+  const tasks = [
+    new Task(1, 3, 2, []),
+    new Task(2, 4, 3, [1]),
+    new Task(3, 2, 4, [1]),
+    new Task(4, 5, 3, [2]),
+    new Task(5, 1, 2, [3]),
+    new Task(6, 2, 3, [3]),
+    new Task(7, 4, 2, [4, 5]),
+    new Task(8, 3, 2, [6, 7]),
+
+  ];
+
+  const expectedChain = [1, 2, 4, 7, 8];
+  const expectedProjectDuration = 19;
+
+  console.log("Result (test-2):");
+
+  const result = optimalSolution(tasks);
+
+  printTaskList(result);
+  printLoadChart(result);
+
+  const outputCriticalChain = getCriticalTaskChain(result).filter(task => !task.isVirtual).map(task => task.taskId)
+  const projectDuration = findProjectDuration(result)
+
+  expect(toStrictEqual1Array(outputCriticalChain, expectedChain)).toBe(true);
+  expect(projectDuration).toBe(expectedProjectDuration);
+});
+
+test("gridPlanningSolution_test-v14", () => {
+  const tasks = [
+    new Task(1, 15, 3, []),
+    new Task(2, 7, 4, []),
+    new Task(3, 5, 2, [2]),
+    new Task(4, 6, 1, [1]),
+    new Task(5, 8, 2, [1]),
+    new Task(6, 6, 2, [3]),
+    new Task(7, 10, 3, [4]),
+    new Task(8, 8, 4, [5, 6]),
+    new Task(9, 9, 5, [3]),
+    new Task(10, 12, 3, [5, 6, 7]),
+    new Task(11, 9, 4, [8, 9]),
+
+  ];
+
+  const expectedChain = [1, 4, 7, 10];
+  const expectedProjectDuration = 43;
+
+  console.log("Result (test-v14):");
+
+  const result = optimalSolution(tasks);
+
+  printTaskList(result);
+  printLoadChart(result);
+
+  const outputCriticalChain = getCriticalTaskChain(result).filter(task => !task.isVirtual).map(task => task.taskId)
+  const projectDuration = findProjectDuration(result)
+
+  expect(toStrictEqual1Array(outputCriticalChain, expectedChain)).toBe(true);
+  expect(projectDuration).toBe(expectedProjectDuration);
+});
